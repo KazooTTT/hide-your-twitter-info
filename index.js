82b1bb362af2db33854fd3423a970a473be17335
KazooTTT <yjwang.echo@gmail.com>
2024-06-29 16:48:48 +0800

    feat: Add a button to control whether to hide or show

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name         Hide your twitter info
 // @namespace    https://github.com/KazooTTT/hide-your-twitter-info
-// @version      0.5
+// @version      0.6
 // @description  Hide your Twitter information. Prevent others from seeing your ID, nickname, and profile picture in public places.
 // @author       KazooTTT
 // @match        https://twitter.com/*
 // @match        https://x.com/*
-// @icon         https://github.com/kazoottt.png
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
 // @license      MIT
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
 // ==/UserScript==
 
 const insertStylesBySelector = (selector) => {
@@ -24,19 +27,51 @@ display:none
 
 (function () {
   "use strict";
-  insertStylesBySelector(`[data-testid="SideNav_AccountSwitcher_Button"] > div:nth-child(1)`);
-  insertStylesBySelector(`[data-testid="SideNav_AccountSwitcher_Button"] > div:nth-child(2)`);
 
-  const timer = setInterval(() => {
-    const accountEl = document.querySelector(
-      'a[data-testid="AppTabBar_Profile_Link"]'
-    );
-    if (accountEl) {
-      const accountId = accountEl.href.split("/").pop();
-      insertStylesBySelector(
-        `div[data-testid="UserAvatar-Container-${accountId}"]`
-      );
-      clearInterval(timer);
+  function useOption(key, title, defaultValue) {
+    if (typeof GM_getValue === 'undefined') {
+      return {
+        value: defaultValue,
+      }
     }
-  }, 1500);
+
+    let value = GM_getValue(key, defaultValue)
+    const ref = {
+      get value() {
+        return value
+      },
+      set value(v) {
+        value = v
+        GM_setValue(key, v)
+        location.reload()
+      },
+    }
+
+    GM_registerMenuCommand(`${title}: ${value ? '✅' : '❌'}`, () => {
+      ref.value = !value
+    })
+
+    return ref
+  }
+
+  const hideTwitterInfo = useOption('hideTwitterInfoTab', 'Hide Twitter Info', true)
+
+  if(hideTwitterInfo.value){
+  
+    insertStylesBySelector(`[data-testid="SideNav_AccountSwitcher_Button"] > div:nth-child(1)`);
+    insertStylesBySelector(`[data-testid="SideNav_AccountSwitcher_Button"] > div:nth-child(2)`);
+
+    const timer = setInterval(() => {
+      const accountEl = document.querySelector(
+        'a[data-testid="AppTabBar_Profile_Link"]'
+      );
+      if (accountEl) {
+        const accountId = accountEl.href.split("/").pop();
+        insertStylesBySelector(
+          `div[data-testid="UserAvatar-Container-${accountId}"]`
+        );
+        clearInterval(timer);
+      }
+    }, 1500);
+  }
 })();
